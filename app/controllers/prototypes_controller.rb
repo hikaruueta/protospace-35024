9928e054887045d93b96e2424c61b912da87ee34
hikaruueta <hkr.u.e.t.0821@gmail.com>
2021-04-05 14:03:48 +0900

    fix

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,13 +1,15 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :delete]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :move_to_index, except: [:index, :show]
 
   def index
     @prototypes = Prototype.all
   end
+
   def new
     @prototype = Prototype.new
   end
+
   def create
     @prototype = Prototype.create(prototype_params)
     if @prototype.save
@@ -52,13 +54,10 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)  
   end
 
-  def set_prototype
-    @prototype = Prototype.find(params[:id])
-  end
   
   def move_to_index
     unless user_signed_in?
-      redirect_to root_path
+      redirect_to action: :index
     end 
   end   
 end
